ffe87e6e417c17a9b8037c02e7a830b75673ad1d
Izan <alvarezvarelaizan@gmail.com>
2025-10-06 18:00:38 +0200

    Error 404 page added

--- app/main.py
+++ app/main.py
@@ -13,16 +13,19 @@ with open(Path("data\pokemon.json"), "r", encoding="utf-8") as f:
     app.config["DATA"] = json.load(f)
 
 
-@app.route('/')
+@app.route('/') #Welcome
 def index():
     return render_template('index.html', music="static/sounds/inicio.mp3", current_year=current_year)
 
 
-@app.route('/pokedex')
+@app.route('/pokedex') #Pokemons list
 def pokedex():
     pokemon_list = current_app.config["DATA"]
     return render_template('pokedex.html', pokemon_list=pokemon_list, current_year=current_year)
 
+@app.route('/404')
+def error404():
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080)
