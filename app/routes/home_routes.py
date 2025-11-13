264c693c1f143eaa1a7f69a3a9d3682d9d475ebd
Axel Cartaya <axelcartaya1@gmail.com>
2025-11-13 13:07:45 +0100

    Fixed: error in trainer session on index

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -7,8 +7,10 @@ home_bp = Blueprint('home', __name__, template_folder='templates')
 current_year = datetime.now().year
 
 
-@home_bp.route('/')  # Welcome
+@home_bp.route('/', methods=["GET"])  # Welcome
 def index():
+    if session.get("invalid_trainer") is None:
+        session.pop("invalid_trainer", None)
     return render_template('index.html', music="static/sounds/inicio.mp3", current_year=current_year)
 
 
@@ -19,11 +21,12 @@ def error404():
 
 @home_bp.route('/trainer', methods=["POST"])
 def trainer():
+    session.pop("invalid_trainer", None)
     session["trainer"] = request.form['trainer']
 
     if (len(session["trainer"]) < 3 or len(session["trainer"]) > 15):
         session["invalid_trainer"] = "The username must have a minimum of 3 characters and a maximum of 15."
         return redirect(url_for("home.index"))
     else:
-        session.pop("invalid_trainer")
+
         return redirect(url_for("pokemon.pokedex"))
